3fef70258e413e9185ba48a8a07f420c2fa87183
gamadjg <gamadavid36@gmail.com>
2023-02-13 14:36:33 -0800

    Projects now rendering on portfolio. Removed grid css from index.html/projects section. main.js typo error when pulling projects. populateProjects creating a div for header, project repo link, image, summary, and tech stack.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ import { default as data } from "./db.js";
 
 import { populateAbout } from "./populateAbout.js";
 import { populateSkills } from "./populateSkills.js";
+import { populateProjects } from "./populateProjects.js";
 
-// populateAbout(data.projects, "projects");
-populateSkills(data.skills, "skills");
 populateAbout(data.bio.about, "about");
+populateSkills(data.skills, "skills");
+populateProjects(data.projects, "projects");
--- assets/js/populateProjects.js
+++ assets/js/populateProjects.js
@@ -1,11 +1,40 @@
 export function populateProjects(items, id) {
-	let aboutTag = document.getElementById(id);
-	let aboutBody = document.createElement("p");
-	aboutBody.innerHTML = items;
+	let projectsTag = document.getElementById(id);
+	for (let i = 0; i < items.length; i++) {
+		let projectName = document.createElement("h3");
+		projectName.innerHTML = items[i].projectName;
+		projectName.className = "text-xl font-semibold mb-0";
 
-	// let aboutSection = document.createElement("div");
-	// aboutSection.className = "mb-5";
-	// aboutSection.append(aboutBody);
+		let repoLink = document.createElement("a");
+		repoLink.href = items[i].repo;
+		repoLink.innerHTML = "(Github)";
 
-	aboutTag.append(aboutBody);
+		let header = document.createElement("div");
+		header.className = "flex items-center";
+		header.append(projectName);
+		header.append(repoLink);
+
+		let image = document.createElement("i");
+		image.className = "fa-sharp fa-solid fa-image w-6/12";
+
+		let summary = document.createElement("p");
+		summary.innerHTML = items[i].summary;
+		let rightBody = document.createElement("div");
+		let skillList = document.createElement("p");
+		skillList.innerHTML = ` Tech: ${items[i].techStack}`;
+
+		rightBody.append(summary);
+		rightBody.append(skillList);
+
+		let body = document.createElement("div");
+		body.className = "flex ";
+		body.append(image);
+		body.append(rightBody);
+
+		let project = document.createElement("div");
+		project.className = "mb-5";
+		project.append(header);
+		project.append(body);
+		projectsTag.append(project);
+	}
 }
