bbe1540cd3717ef631cc1e1178c73c1d8d681386
gamadjg <gamadavid36@gmail.com>
2023-02-14 09:14:33 -0800

    Updated main.js to import and call function for populateEducation

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,10 @@ import { populateAbout } from "./populateAbout.js";
 import { populateSkills } from "./populateSkills.js";
 import { populateProjects } from "./populateProjects.js";
 import { populateWork } from "./populateWork.js";
+import { populateEducation } from "./populateEducation.js";
 
 populateAbout(data.bio.about, "about");
 populateSkills(data.skills, "skills");
 populateProjects(data.projects, "projects");
 populateWork(data.experience, "experience");
+populateEducation(data.education, "education");
---
+++ assets/js/populateEducation.js
@@ -0,0 +1,50 @@
+export function populateEducation(items, id) {
+	let skillsTag = document.getElementById(id);
+	for (let i = 0; i < items.length; i++) {
+		let title = document.createElement("h3");
+		title.className = "text-xl font-semibold mb-0";
+		title.innerHTML = items[i].title;
+
+		let duration = document.createElement("p");
+		duration.className = "";
+		duration.innerHTML = items[i].duration;
+
+		let header = document.createElement("div");
+		header.className = "flex items-center justify-between";
+		header.append(title);
+		header.append(duration);
+
+		let location = document.createElement("p");
+		location.className = "";
+		location.innerHTML = items[i].location;
+
+		let detailsList = document.createElement("ul");
+		for (let x = 0; x < items[i].details.length; x++) {
+			let listItem = document.createElement("li");
+			listItem.innerHTML = items[i].details[x];
+			detailsList.append(listItem);
+		}
+
+		let tags = document.createElement("div");
+		tags.className = "flex flex-wrap gap-5";
+		for (let x = 0; x < items[i].tags.length; x++) {
+			let tag = document.createElement("p");
+			tag.className = "";
+			tag.innerHTML = items[i].tags[x];
+			tags.append(tag);
+		}
+
+		let body = document.createElement("div");
+		body.className = "";
+		body.append(location);
+		body.append(detailsList);
+		body.append(tags);
+
+		let educationContainer = document.createElement("div");
+		educationContainer.className = "mb-5";
+		educationContainer.append(header);
+		educationContainer.append(body);
+
+		skillsTag.append(educationContainer);
+	}
+}
