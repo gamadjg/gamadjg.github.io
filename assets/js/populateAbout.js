934641ad679471e62083ba404edc5e8cc4b621da
gamadjg <gamadavid36@gmail.com>
2023-02-20 12:44:22 -0800

    Converted elements that do not get re-assigned into const's. Converted body element into about div container and added background, rounded edges, and padding.

--- assets/js/populateAbout.js
+++ assets/js/populateAbout.js
@@ -1,12 +1,14 @@
 export function populateAbout(items, id) {
-	let sectionTitle = document.createElement("h2");
+	const container = document.getElementById(id);
+
+	const sectionTitle = document.createElement("h2");
 	sectionTitle.className = "text-4xl font-semibold";
 	sectionTitle.innerHTML = "About";
 
-	let body = document.createElement("p");
-	body.innerHTML = items;
+	const aboutContainer = document.createElement("div");
+	aboutContainer.className = "bg-gray-300 rounded p-5";
+	aboutContainer.innerHTML = items;
 
-	let container = document.getElementById(id);
 	container.append(sectionTitle);
-	container.append(body);
+	container.append(aboutContainer);
 }
