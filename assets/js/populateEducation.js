162aa405bcf43840e3e7d05704ef60d50238dbe5
gamadjg <gamadavid36@gmail.com>
2023-02-20 11:39:23 -0800

    Converted elements that do not get re-assigned into const's. Converted tags list into unordered list. Created eduList to add each education item, to then apply css to eduList

--- assets/js/populateEducation.js
+++ assets/js/populateEducation.js
@@ -1,55 +1,58 @@
 export function populateEducation(items, id) {
-	let container = document.getElementById(id);
-	let sectionTitle = document.createElement("h2");
+	const container = document.getElementById(id);
+	const sectionTitle = document.createElement("h2");
 	sectionTitle.className = "text-4xl font-semibold";
 	sectionTitle.innerHTML = "Education";
 	container.append(sectionTitle);
 
-	for (let i = 0; i < items.length; i++) {
-		let title = document.createElement("h3");
+	const eduList = document.createElement("div");
+	eduList.className = "bg-gray-300 rounded p-5";
+
+	for (let eduIndex = 0; eduIndex < items.length; eduIndex++) {
+		const title = document.createElement("h3");
 		title.className = "text-xl font-semibold mb-0";
-		title.innerHTML = items[i].title;
+		title.innerHTML = items[eduIndex].title;
 
-		let duration = document.createElement("p");
+		const duration = document.createElement("p");
 		duration.className = "";
-		duration.innerHTML = items[i].duration;
+		duration.innerHTML = items[eduIndex].duration;
 
-		let header = document.createElement("div");
+		const header = document.createElement("div");
 		header.className = "flex items-center justify-between";
 		header.append(title);
 		header.append(duration);
 
-		let location = document.createElement("p");
+		const location = document.createElement("p");
 		location.className = "";
-		location.innerHTML = items[i].location;
+		location.innerHTML = items[eduIndex].location;
 
-		let detailsList = document.createElement("ul");
-		for (let x = 0; x < items[i].details.length; x++) {
-			let listItem = document.createElement("li");
-			listItem.innerHTML = items[i].details[x];
-			detailsList.append(listItem);
+		const details = document.createElement("ul");
+		details.className = "list-disc pl-5";
+		for (let index = 0; index < items[eduIndex].details.length; index++) {
+			const listItem = document.createElement("li");
+			listItem.innerHTML = items[eduIndex].details[index];
+			details.append(listItem);
 		}
 
-		let tags = document.createElement("div");
-		tags.className = "flex flex-wrap gap-5";
-		for (let x = 0; x < items[i].tags.length; x++) {
-			let tag = document.createElement("p");
-			tag.className = "";
-			tag.innerHTML = items[i].tags[x];
+		const tags = document.createElement("ul");
+		tags.className = "flex flex-wrap";
+		for (let index = 0; index < items[eduIndex].tags.length; index++) {
+			const tag = document.createElement("li");
+			tag.className = "bg-teal-500 text-white rounded p-1 m-1";
+			tag.innerHTML = items[eduIndex].tags[index];
 			tags.append(tag);
 		}
 
-		let body = document.createElement("div");
-		body.className = "";
+		const body = document.createElement("div");
 		body.append(location);
-		body.append(detailsList);
+		body.append(details);
 		body.append(tags);
 
-		let educationContainer = document.createElement("div");
-		educationContainer.className = "mb-5";
-		educationContainer.append(header);
-		educationContainer.append(body);
+		const eduItem = document.createElement("div");
+		eduItem.append(header);
+		eduItem.append(body);
 
-		container.append(educationContainer);
+		eduList.append(eduItem);
 	}
+	container.append(eduList);
 }
