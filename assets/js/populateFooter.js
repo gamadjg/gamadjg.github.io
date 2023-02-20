725f956967e69335b2bbd172e9c6930b49d82d3d
gamadjg <gamadavid36@gmail.com>
2023-02-20 12:32:43 -0800

    Moved all appends to bottom of file. Added background color and rounded edges to footer container. Replaced left footer 'test' with contact.email. Updated items array argument with new items.profiles object

--- assets/js/populateFooter.js
+++ assets/js/populateFooter.js
@@ -2,27 +2,27 @@ export function populateFooter(items, id) {
 	const container = document.getElementById(id);
 
 	const footer = document.createElement("div");
-	footer.className = "flex justify-between";
+	footer.className = "flex justify-between bg-gray-300 rounded p-5";
 
 	const leftFooter = document.createElement("div");
 	leftFooter.className = "flex items-center justify-center";
-	leftFooter.innerHTML = "test";
-	footer.append(leftFooter);
+	leftFooter.innerHTML = `Contact: ${items.contact.email}`;
 
 	const rightFooter = document.createElement("div");
 	rightFooter.className = "flex justify-between gap-5";
-
-	for (let i = 0; i < items.length; i++) {
+	for (let i = 0; i < items.profiles.length; i++) {
 		const image = document.createElement("img");
-		image.src = `./assets/images/${items[i].icon}.png`;
-		image.alt = `${items[i].icon} thumbnail`;
+		image.src = `./assets/images/${items.profiles[i].icon}.png`;
+		image.alt = `${items.profiles[i].icon} thumbnail`;
 		image.className = "w-8";
 
 		const link = document.createElement("a");
-		link.href = items[i].link;
+		link.href = items.profiles[i].link;
 		link.append(image);
 		rightFooter.append(link);
 	}
+
+	footer.append(leftFooter);
 	footer.append(rightFooter);
 	container.append(footer);
 }
